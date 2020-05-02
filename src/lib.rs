b99a878717c2de4f6dfc1bbe96247d155a67062f
Franck Royer <franck@royer.one>
2020-05-02 19:11:28 +1000

    Use tabs

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 #![warn(
-    unused_extern_crates,
-    missing_debug_implementations,
-    missing_copy_implementations,
-    rust_2018_idioms,
-    clippy::cast_possible_truncation,
-    clippy::cast_sign_loss,
-    clippy::fallible_impl_from,
-    clippy::cast_precision_loss,
-    clippy::cast_possible_wrap,
-    clippy::print_stdout,
-    clippy::dbg_macro
+	unused_extern_crates,
+	missing_debug_implementations,
+	missing_copy_implementations,
+	rust_2018_idioms,
+	clippy::cast_possible_truncation,
+	clippy::cast_sign_loss,
+	clippy::fallible_impl_from,
+	clippy::cast_precision_loss,
+	clippy::cast_possible_wrap,
+	clippy::print_stdout,
+	clippy::dbg_macro
 )]
 #![forbid(unsafe_code)]
 
@@ -21,55 +21,55 @@ use yew::prelude::*;
 mod session;
 
 struct Model {
-    sessions: Vec<Session>,
+	sessions: Vec<Session>,
 }
 
 impl Component for Model {
-    type Message = ();
-    type Properties = ();
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Model::default()
-    }
+	type Message = ();
+	type Properties = ();
+	fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+		Model::default()
+	}
 
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
-        true
-    }
+	fn update(&mut self, _: Self::Message) -> ShouldRender {
+		true
+	}
 
-    fn change(&mut self, _: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
-        false
-    }
+	fn change(&mut self, _: Self::Properties) -> ShouldRender {
+		// Should only return "true" if new properties are different to
+		// previously received properties.
+		// This component has no properties so we will always return "false".
+		false
+	}
 
-    fn view(&self) -> Html {
-        web_sys::console::log_1(&"Rendering Model".into());
-        html! {
-            <div>
-                { for self.sessions.iter().map(|s| s.view() ) }
-            </div>
-        }
-    }
+	fn view(&self) -> Html {
+		web_sys::console::log_1(&"Rendering Model".into());
+		html! {
+			<div>
+				{ for self.sessions.iter().map(|s| s.view() ) }
+			</div>
+		}
+	}
 }
 
 impl Default for Model {
-    fn default() -> Self {
-        Self {
-            sessions: vec![
-                Session {
-                    club: "UTS Jitsu".to_string(),
-                    schedule: Schedule::new(Weekday::Wed, NaiveTime::from_hms(19, 15, 00)),
-                },
-                Session {
-                    club: "London Green +".to_string(),
-                    schedule: Schedule::new(Weekday::Sat, NaiveTime::from_hms(11, 15, 00)),
-                },
-            ],
-        }
-    }
+	fn default() -> Self {
+		Self {
+			sessions: vec![
+				Session {
+					club: "UTS Jitsu".to_string(),
+					schedule: Schedule::new(Weekday::Wed, NaiveTime::from_hms(19, 15, 00)),
+				},
+				Session {
+					club: "London Green +".to_string(),
+					schedule: Schedule::new(Weekday::Sat, NaiveTime::from_hms(11, 15, 00)),
+				},
+			],
+		}
+	}
 }
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
-    App::<Model>::new().mount_to_body();
+	App::<Model>::new().mount_to_body();
 }
--- src/session.rs
+++ src/session.rs
@@ -2,201 +2,201 @@ use chrono::{DateTime, Datelike, Duration, NaiveTime, TimeZone, Timelike, Utc, W
 use yew::{prelude::*, Component, ComponentLink, Html, ShouldRender};
 
 impl Component for Session {
-    type Message = ();
-    type Properties = ();
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        unimplemented!()
-    }
+	type Message = ();
+	type Properties = ();
+	fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+		unimplemented!()
+	}
 
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
-        // Nothing to do here
-        true
-    }
+	fn update(&mut self, _: Self::Message) -> ShouldRender {
+		// Nothing to do here
+		true
+	}
 
-    fn change(&mut self, _: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
-        false
-    }
+	fn change(&mut self, _: Self::Properties) -> ShouldRender {
+		// Should only return "true" if new properties are different to
+		// previously received properties.
+		// This component has no properties so we will always return "false".
+		false
+	}
 
-    fn view(&self) -> Html {
-        web_sys::console::log_1(&"view on session".into());
-        let text = format!("{}: {}", self.club, self.schedule.next());
-        web_sys::console::log_1(&"generated text".into());
-        web_sys::console::log_1(&text.clone().into());
-        let html = html! {
-            <div>
-                <p>{ text }</p>
-            </div>
-        };
-        web_sys::console::log_1(&"Session rendered".into());
-        html
-    }
+	fn view(&self) -> Html {
+		web_sys::console::log_1(&"view on session".into());
+		let text = format!("{}: {}", self.club, self.schedule.next());
+		web_sys::console::log_1(&"generated text".into());
+		web_sys::console::log_1(&text.clone().into());
+		let html = html! {
+			<div>
+				<p>{ text }</p>
+			</div>
+		};
+		web_sys::console::log_1(&"Session rendered".into());
+		html
+	}
 }
 
 pub struct Session {
-    pub club: String,
-    pub schedule: Schedule,
+	pub club: String,
+	pub schedule: Schedule,
 }
 
 pub struct Schedule {
-    weekday: Weekday,
-    time: NaiveTime,
+	weekday: Weekday,
+	time: NaiveTime,
 }
 
 impl Schedule {
-    pub fn new(weekday: Weekday, time: NaiveTime) -> Schedule {
-        Schedule { weekday, time }
-    }
+	pub fn new(weekday: Weekday, time: NaiveTime) -> Schedule {
+		Schedule { weekday, time }
+	}
 
-    pub fn time_to_next(&self) -> Duration {
-        self.time_to_next_from_current(Utc::now())
-    }
+	pub fn time_to_next(&self) -> Duration {
+		self.time_to_next_from_current(Utc::now())
+	}
 
-    pub fn next(&self) -> DateTime<Utc> {
-        web_sys::console::log_1(&"Calling now".into());
-        let now = Utc::now();
-        web_sys::console::log_1(&"now has been called".into());
-        self.next_from_current(now)
-    }
+	pub fn next(&self) -> DateTime<Utc> {
+		web_sys::console::log_1(&"Calling now".into());
+		let now = Utc::now();
+		web_sys::console::log_1(&"now has been called".into());
+		self.next_from_current(now)
+	}
 
-    pub fn time_to_next_from_current(&self, time: DateTime<Utc>) -> Duration {
-        let next = self.next_from_current(time);
-        next - time
-    }
+	pub fn time_to_next_from_current(&self, time: DateTime<Utc>) -> Duration {
+		let next = self.next_from_current(time);
+		next - time
+	}
 
-    /// Calculate when the next session should be based
-    /// on the current time.
-    pub fn next_from_current(&self, current_time: DateTime<Utc>) -> DateTime<Utc> {
-        web_sys::console::log_1(&"entering next_from_current".into());
-        let weekday = self.weekday.num_days_from_monday();
-        let current_weekday = current_time.weekday().num_days_from_monday();
+	/// Calculate when the next session should be based
+	/// on the current time.
+	pub fn next_from_current(&self, current_time: DateTime<Utc>) -> DateTime<Utc> {
+		web_sys::console::log_1(&"entering next_from_current".into());
+		let weekday = self.weekday.num_days_from_monday();
+		let current_weekday = current_time.weekday().num_days_from_monday();
 
-        let days_to = if current_weekday <= weekday {
-            weekday - current_weekday
-        } else {
-            7 + weekday - current_weekday
-        };
+		let days_to = if current_weekday <= weekday {
+			weekday - current_weekday
+		} else {
+			7 + weekday - current_weekday
+		};
 
-        let mut next = Utc
-            .ymd(
-                current_time.year(),
-                current_time.month(),
-                current_time.day(),
-            )
-            .and_hms(self.time.hour(), self.time.minute(), self.time.second());
-        next = next + Duration::days(days_to as i64);
-        if next < current_time {
-            // Session was today
-            next = next + Duration::days(7)
-        }
-        next
-    }
+		let mut next = Utc
+			.ymd(
+				current_time.year(),
+				current_time.month(),
+				current_time.day(),
+			)
+			.and_hms(self.time.hour(), self.time.minute(), self.time.second());
+		next = next + Duration::days(days_to as i64);
+		if next < current_time {
+			// Session was today
+			next = next + Duration::days(7)
+		}
+		next
+	}
 }
 
 #[cfg(test)]
 impl Default for Session {
-    fn default() -> Self {
-        Session {
-            club: "UTS Jitsu".to_string(),
-            schedule: Schedule::new(Weekday::Wed, NaiveTime::from_hms(19, 15, 00)),
-        }
-    }
+	fn default() -> Self {
+		Session {
+			club: "UTS Jitsu".to_string(),
+			schedule: Schedule::new(Weekday::Wed, NaiveTime::from_hms(19, 15, 00)),
+		}
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn next_session_tomorrow_same_time() {
-        // Saturday 2nd of May
-        let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
+	#[test]
+	fn next_session_tomorrow_same_time() {
+		// Saturday 2nd of May
+		let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
 
-        let schedule = Schedule::new(Weekday::Sun, NaiveTime::from_hms(10, 0, 0));
+		let schedule = Schedule::new(Weekday::Sun, NaiveTime::from_hms(10, 0, 0));
 
-        assert_eq!(
-            schedule.next_from_current(current_time),
-            Utc.ymd(2020, 5, 3).and_hms(10, 00, 00)
-        );
-    }
+		assert_eq!(
+			schedule.next_from_current(current_time),
+			Utc.ymd(2020, 5, 3).and_hms(10, 00, 00)
+		);
+	}
 
-    #[test]
-    fn next_session_in_six_days_same_time() {
-        // Saturday 2nd of May
-        let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
+	#[test]
+	fn next_session_in_six_days_same_time() {
+		// Saturday 2nd of May
+		let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
 
-        let schedule = Schedule::new(Weekday::Fri, NaiveTime::from_hms(10, 0, 0));
+		let schedule = Schedule::new(Weekday::Fri, NaiveTime::from_hms(10, 0, 0));
 
-        assert_eq!(
-            schedule.next_from_current(current_time),
-            Utc.ymd(2020, 5, 8).and_hms(10, 00, 00)
-        );
-    }
+		assert_eq!(
+			schedule.next_from_current(current_time),
+			Utc.ymd(2020, 5, 8).and_hms(10, 00, 00)
+		);
+	}
 
-    #[test]
-    fn next_session_in_six_days_twenty_three_hours() {
-        // Saturday 2nd of May
-        let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
+	#[test]
+	fn next_session_in_six_days_twenty_three_hours() {
+		// Saturday 2nd of May
+		let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
 
-        let schedule = Schedule::new(Weekday::Sat, NaiveTime::from_hms(9, 0, 0));
+		let schedule = Schedule::new(Weekday::Sat, NaiveTime::from_hms(9, 0, 0));
 
-        assert_eq!(
-            schedule.next_from_current(current_time),
-            Utc.ymd(2020, 5, 9).and_hms(9, 00, 00)
-        );
-    }
+		assert_eq!(
+			schedule.next_from_current(current_time),
+			Utc.ymd(2020, 5, 9).and_hms(9, 00, 00)
+		);
+	}
 
-    #[test]
-    fn one_day_to_next_session() {
-        // Saturday 2nd of May
-        let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
+	#[test]
+	fn one_day_to_next_session() {
+		// Saturday 2nd of May
+		let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
 
-        let schedule = Schedule::new(Weekday::Sun, NaiveTime::from_hms(10, 0, 0));
+		let schedule = Schedule::new(Weekday::Sun, NaiveTime::from_hms(10, 0, 0));
 
-        assert_eq!(
-            schedule.time_to_next_from_current(current_time),
-            Duration::days(1)
-        );
-    }
+		assert_eq!(
+			schedule.time_to_next_from_current(current_time),
+			Duration::days(1)
+		);
+	}
 
-    #[test]
-    fn one_hour_to_next_session() {
-        // Saturday 2nd of May
-        let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
+	#[test]
+	fn one_hour_to_next_session() {
+		// Saturday 2nd of May
+		let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
 
-        let schedule = Schedule::new(Weekday::Sat, NaiveTime::from_hms(11, 0, 0));
+		let schedule = Schedule::new(Weekday::Sat, NaiveTime::from_hms(11, 0, 0));
 
-        assert_eq!(
-            schedule.time_to_next_from_current(current_time),
-            Duration::hours(1)
-        );
-    }
+		assert_eq!(
+			schedule.time_to_next_from_current(current_time),
+			Duration::hours(1)
+		);
+	}
 
-    #[test]
-    fn three_days_to_next_session_next_week() {
-        // Saturday 2nd of May
-        let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
+	#[test]
+	fn three_days_to_next_session_next_week() {
+		// Saturday 2nd of May
+		let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
 
-        let schedule = Schedule::new(Weekday::Tue, NaiveTime::from_hms(10, 0, 0));
+		let schedule = Schedule::new(Weekday::Tue, NaiveTime::from_hms(10, 0, 0));
 
-        assert_eq!(
-            schedule.time_to_next_from_current(current_time),
-            Duration::days(3)
-        );
-    }
+		assert_eq!(
+			schedule.time_to_next_from_current(current_time),
+			Duration::days(3)
+		);
+	}
 
-    #[test]
-    fn six_days_twenty_three_hours_to_next_session() {
-        // Saturday 2nd of May
-        let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
+	#[test]
+	fn six_days_twenty_three_hours_to_next_session() {
+		// Saturday 2nd of May
+		let current_time = Utc.ymd(2020, 5, 2).and_hms(10, 00, 00);
 
-        let schedule = Schedule::new(Weekday::Sat, NaiveTime::from_hms(09, 0, 0));
+		let schedule = Schedule::new(Weekday::Sat, NaiveTime::from_hms(09, 0, 0));
 
-        assert_eq!(
-            schedule.time_to_next_from_current(current_time),
-            Duration::days(6) + Duration::hours(23)
-        );
-    }
+		assert_eq!(
+			schedule.time_to_next_from_current(current_time),
+			Duration::days(6) + Duration::hours(23)
+		);
+	}
 }
