f5614cf339dab5d84d159de9fa2563fc0ddb01fe
Franck Royer <franck@royer.one>
2020-05-04 16:32:53 +1000

    Cleanup data

--- src/data.rs
+++ src/data.rs
@@ -11,6 +11,321 @@ pub const DATA: &str = r#"
       "link": "https://www.facebook.com/groups/UniversitiesBrightonJitsu/",
       "weekday": "Wed",
       "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/AstonJiuJitsu/",
+      "name": "Aston University Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "2030-2130"
+    }, {
+      "link": "https://www.facebook.com/groups/UniversitiesBrightonJitsu/",
+      "name": "Brighton Jiu Jitsu Club",
+      "weeday": "Wed",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/bruneljitsu/",
+      "name": "Brunel University Jiu Jitsu Club",
+      "weekday": "Wed",
+      "start": "1800"
+    }, {
+      "link": "https://www.facebook.com/groups/7674804500/",
+      "name": "Coventry University Jiu Jitsu Club",
+      "weekday": "Wed"
+    }, {
+      "link": "https://www.facebook.com/newquayjitsu/",
+      "name": "Cornwall Jiu Jitsu Club",
+      "weeday": "Mon",
+      "start": "1745"
+    }, {
+      "link": "https://www.facebook.com/groups/835506009825760",
+      "name": "Edinburgh University Jiu Jitsu Club",
+      "weekday": "Wed",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/835506009825760",
+      "name": "Edinburgh University Jiu Jitsu Club",
+      "weekday": "Sat",
+      "start": "1300"
+    }, {
+      "link": "https://www.facebook.com/groups/835506009825760",
+      "name": "Edinburgh Napier University Jiu Jitsu Club",
+      "weekday": "Wed",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/835506009825760",
+      "name": "Edinburgh Napier University Jiu Jitsu Club",
+      "weekday": "Sat",
+      "start": "1300"
+    }, {
+      "link": "https://www.facebook.com/groups/surreyjitsu/",
+      "name": "Guildford Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "1930"
+    }, {
+      "link": "https://www.facebook.com/groups/surreyjitsu/",
+      "name": "Guildford Jiu Jitsu Club",
+      "weekday": "Thu",
+      "start": "1930"
+    }, {
+      "link": "https://www.facebook.com/groups/ImperialJitsu/",
+      "name": "Imperial College London Jiu Jitsu Club",
+      "weekday": "Sun"
+    }, {
+      "link": "https://www.facebook.com/groups/kingsjitsu/",
+      "name": "Kings College London Jiu Jitsu Club",
+      "weekday": "Tue"
+    }, {
+      "link": "https://www.facebook.com/groups/kingsjitsu/",
+      "name": "Kings College London Jiu Jitsu Club",
+      "weekday": "Sun"
+    }, {
+      "link": "https://www.facebook.com/groups/kingsjitsu/",
+      "name": "Kings College London Jiu Jitsu Club",
+      "weekday": "Thu"
+    }, {
+      "link": "https://www.facebook.com/LUUJiuJitsuClub/",
+      "name": "Leeds University Union Jiu Jitsu Club",
+      "weekday": "Mon"
+    }, {
+      "link": "https://www.facebook.com/groups/LSEAUjitsu/",
+      "name": "London School of Economics Jiu Jitsu Club",
+      "weekday": "Wed"
+    }, {
+      "link": "https://www.facebook.com/groups/TJFLondonPurpleAndAbove/",
+      "name": "London Green plus",
+      "weekday": "Wed",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/TJFLondonPurpleAndAbove/",
+      "name": "London Green plus",
+      "weekday": "Sat",
+      "start": "1100"
+    }, {
+      "link": "https://www.facebook.com/LSUJitsu/",
+      "name": "Loughborough Students Jiu Jitsu Club",
+      "weekday": "Thu",
+      "start": "2000"
+    }, {
+      "link": "https://www.facebook.com/groups/uomjitsu/",
+      "name": "Manchester Metropolitan University Jiu Jitsu Club",
+      "weekday": "Mon"
+    }, {
+      "link": "https://www.facebook.com/Moorgate-Jitsu-465289956820955/",
+      "name": "Morgate Jiu Jitsu Club",
+      "weekday": "Thu"
+    }, {
+      "link": "https://www.facebook.com/TeamNCJJ/",
+      "name": "Newcastle City Jiu Jitsu Club",
+      "weekday": "Mon"
+    }, {
+      "link": "https://www.facebook.com/TeamNCJJ/",
+      "name": "Newcastle City Jiu Jitsu Club",
+      "weekday": "Fri"
+    }, {
+      "link": "https://www.facebook.com/newquayjitsu/",
+      "name": "Newquay Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "1745"
+    }, {
+      "link": "https://www.facebook.com/jiujitsunorwich/",
+      "name": "Norwich Jiu Jitsu Club",
+      "weekday": "Tue",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/jiujitsunorwich/",
+      "name": "Norwich Jiu Jitsu Club",
+      "weekday": "Thu",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/132567286816265/",
+      "name": "Oxford University Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/132567286816265/",
+      "name": "Oxford University Jiu Jitsu Club",
+      "weekday": "Wed",
+      "start": "1800"
+    }, {
+      "link": "https://www.facebook.com/groups/132567286816265/",
+      "name": "Oxford Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/132567286816265/",
+      "name": "Oxford Jiu Jitsu Club",
+      "weekday": "Wed",
+      "start": "1800"
+    }, }, {
+      "link": "https://www.facebook.com/groups/prestonjitsu/",
+      "name": "Preston Jiu Jitsu Club",
+      "weekday": "Tue",
+      "start": "1800"
+    }, {
+      "link": "https://www.facebook.com/Sheffield-Jitsu-Club-108109495961794/",
+      "name": "Sheffield Jiu Jitsu Club",
+      "weekday": "Mon"
+    }, {
+      "link": "https://www.facebook.com/Sheffield-Jitsu-Club-108109495961794/",
+      "name": "Sheffield Jiu Jitsu Club",
+      "weekday": "Wed"
+    }, {
+      "link": "https://www.facebook.com/SouthamptonJitsuClub/",
+      "name": "Southampton Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "1915"
+    }, {
+      "link": "https://www.facebook.com/SouthamptonJitsuClub/",
+      "name": "Southampton Jiu Jitsu Club",
+      "weekday": "Thu",
+      "start": "1915"
+    }, {
+      "link": "https://www.facebook.com/utsjitsu/",
+      "name": "UTS Jitsu",
+      "weekday": "Wed",
+      "start": "0930"
+    }, {
+      "link": "https://www.facebook.com/groups/tadley.jitsu/",
+      "name": "Tadley Jiu Jitsu Club, Hampshire",
+      "weekday": "Mon",
+      "start": "1800"
+    }, {
+      "link": "https://www.facebook.com/groups/UCLJitsu/",
+      "name": "University College London Jiu Jitsu Club",
+      "weekday": "Fri"
+    }, {
+      "link": "https://www.facebook.com/UCBJitsu/",
+      "name": "University College Birmingham Jiu Jitsu Club",
+      "weekday": "Mon"
+    }, {
+      "link": "https://www.facebook.com/UCBJitsu/",
+      "name": "University College Birmingham Jiu Jitsu Club",
+      "weekday": "Thu"
+    }, {
+      "link": "https://www.facebook.com/UniversityOfNorthamptonJitsuka/",
+      "name": "University of Northampton Jiu Jitsu Club",
+      "weekday": "Thu"
+    }, {
+      "link": "https://www.facebook.com/BathUniJitsu/",
+      "name": "University of Bath Jiu Jitsu Club",
+      "weekday": "Wed"
+    }, {
+      "link": "https://www.facebook.com/uobjitsu/",
+      "name": "University of Birmingham Jiu Jitsu Club",
+      "weekday": "Wed",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/2204517862/",
+      "name": "University of Bristol Jiu Jitsu Club",
+      "weekday": "Mon"
+    }, {
+      "link": "https://www.facebook.com/groups/2204517862/",
+      "name": "University of Bristol Jiu Jitsu Club",
+      "weekday": "Wed"
+    }, {
+      "link": "https://www.facebook.com/groups/2204517862/",
+      "name": "University of Bristol Jiu Jitsu Club",
+      "weekday": "Fri"
+    }, {
+      "link": "https://www.facebook.com/groups/UniversitiesBrightonJitsu/",
+      "name": "University of Brighton Jiu Jitsu Club",
+      "weekday": "Wed",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/CUJJC/",
+      "name": "University of Cambridge Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "1930"
+    }, {
+      "link": "https://www.facebook.com/CUJJC/",
+      "name": "University of Cambridge Jiu Jitsu Club",
+      "weekday": "Sat",
+      "start": "1630"
+    }, {
+      "link": "https://www.facebook.com/groups/prestonjitsu/",
+      "name": "University of Central Lancashire Jiu Jitsu Club",
+      "weekday": "Tue",
+      "start": "1800"
+    }, {
+      "link": "https://www.facebook.com/uogjiujitsu/",
+      "name": "University of Glouchester Jiu Jitsu Club",
+      "weekday": "Fri"
+    }, {
+      "link": "https://www.facebook.com/groups/uomjitsu/",
+      "name": "University of Manchester Jiu Jitsu Club",
+      "weekday": "Mon"
+    }, {
+      "link": "https://www.facebook.com/newcastleunijitsu/",
+      "name": "Newcastle University Jiu Jitsu Club",
+      "weekday": "Mon"
+    }, {
+      "link": "https://www.facebook.com/newcastleunijitsu/",
+      "name": "Newcastle University Jiu Jitsu Club",
+      "weekday": "Thu"
+    }, {
+      "link": "https://www.facebook.com/uonjiujitsu/",
+      "name": "University of Nottingham Jiu Jitsu Club",
+      "weekday": "Wed"
+    }, {
+      "link": "https://www.facebook.com/uonjiujitsu/",
+      "name": "University of Nottingham Jiu Jitsu Club",
+      "weekday": "Fri"
+    }, {
+      "link": "https://www.facebook.com/ReadingKnightsJitsu/",
+      "name": "University of Reading Jiu Jitsu Club",
+      "weekday": "Thu",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/ShefUniJiuJitsu/",
+      "name": "University of Sheffield Jiu Jitsu Club",
+      "weekday": "Thu"
+    }, {
+      "link": "https://www.facebook.com/groups/surreyjitsu/",
+      "name": "University of Surrey Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "1930"
+    }, {
+      "link": "https://www.facebook.com/groups/surreyjitsu/",
+      "name": "University of Surrey Jiu Jitsu Club",
+      "weekday": "Thu",
+      "start": "1930"
+    }, {
+      "link": "https://www.facebook.com/groups/UWEJITSUCLUB/",
+      "name": "University of the West of England Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/UWEJITSUCLUB/",
+      "name": "University of the West of England Jiu Jitsu Club",
+      "weekday": "Wed",
+      "start": "1900"
+    }, {
+      "link": "https://www.facebook.com/groups/swanseajitsu/",
+      "name": "Swansea University Jiu Jitsu Club",
+      "weekday": "Wed",
+      "start": "1800"
+    }, {
+      "link": "https://www.facebook.com/WarwickJiuJitsu/",
+      "name": "University of Warwick Jiu Jitsu Club",
+      "weekday": "Mon",
+      "start": "1830"
+    }, {
+      "link": "https://www.facebook.com/WarwickJiuJitsu/",
+      "name": "University of Warwick Jiu Jitsu Club",
+      "weekday": "Thu",
+      "start": "1830"
+    }, {
+      "link": "https://www.facebook.com/groups/106033342784971/",
+      "name": "University of York Jiu Jitsu Club",
+      "weekday": "Mon"
+    }, {
+      "link": "https://www.facebook.com/groups/106033342784971/",
+      "name": "University of York Jiu Jitsu Club",
+      "weekday": "Fri"
+    }, {
+      "link": "https://www.facebook.com/groups/1540807069471782/",
+      "name": "Vienna Jiu Jitsu Club",
+      "weekday": "Tue",
+      "start": "1900"
     }
   ]
 }
