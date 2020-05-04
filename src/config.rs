b6271dec6bd5614a13586845204322f6eb99185f
Franck Royer <franck@royer.one>
2020-05-04 15:15:30 +1000

    Deserialize config file

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,8 @@ use chrono::{NaiveTime, Weekday};
 use serde::{Deserialize, Deserializer};
 
 #[cfg(target_arch = "wasm32")]
+use wasm_bindgen::__rt::std::fmt;
+#[cfg(target_arch = "wasm32")]
 use wasm_bindgen::__rt::std::fs::File;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::__rt::std::io::BufReader;
@@ -9,18 +11,20 @@ use wasm_bindgen::__rt::std::io::BufReader;
 use wasm_bindgen::__rt::std::path::Path;
 
 #[cfg(not(target_arch = "wasm32"))]
+use std::fmt;
+#[cfg(not(target_arch = "wasm32"))]
 use std::fs::File;
 #[cfg(not(target_arch = "wasm32"))]
 use std::io::BufReader;
 #[cfg(not(target_arch = "wasm32"))]
 use std::path::Path;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 struct Config {
 	sessions: Vec<Session>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct Session {
 	name: String,
 	link: String,
@@ -41,16 +45,87 @@ impl Config {
 	}
 }
 
-fn deserialize_weekday<'de, D>(d: D) -> Result<Weekday, D::Error>
+fn deserialize_weekday<'de, D>(deserializer: D) -> Result<Weekday, D::Error>
 where
 	D: Deserializer<'de>,
 {
-	unimplemented!()
+	struct Visitor;
+
+	impl<'de> serde::de::Visitor<'de> for Visitor {
+		type Value = Weekday;
+
+		fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+			formatter.write_str("A capitalized 3-letters weekday")
+		}
+
+		fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+		where
+			E: serde::de::Error,
+		{
+			use chrono::Weekday::*;
+			match v {
+				"Mon" => Ok(Mon),
+				"Tue" => Ok(Tue),
+				"Wed" => Ok(Wed),
+				"Thu" => Ok(Thu),
+				"Fri" => Ok(Fri),
+				"Sat" => Ok(Sat),
+				"Sun" => Ok(Sun),
+				unknown => Err(E::custom(format!("unknown weekday {}", unknown))),
+			}
+		}
+	}
+
+	deserializer.deserialize_str(Visitor)
 }
 
-fn deserialize_time<'de, D>(d: D) -> Result<NaiveTime, D::Error>
+fn deserialize_time<'de, D>(deserializer: D) -> Result<NaiveTime, D::Error>
 where
 	D: Deserializer<'de>,
 {
-	unimplemented!()
+	struct Visitor;
+
+	impl<'de> serde::de::Visitor<'de> for Visitor {
+		type Value = NaiveTime;
+
+		fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+			formatter.write_str("A 24-hours time string: HHMM")
+		}
+
+		fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+		where
+			E: serde::de::Error,
+		{
+			NaiveTime::parse_from_str(v, "%H%M")
+				.map_err(|e| (E::custom(format!("Could not parse time from {}: {}", v, e))))
+		}
+	}
+
+	deserializer.deserialize_str(Visitor)
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn deserialize_config() {
+		let config = Config::read("./static/config.json").unwrap();
+		assert_eq!(config, Config {
+			sessions: vec![
+				Session {
+					name: "Aston University Jiu Jitsu Club".to_string(),
+					link: "https://www.facebook.com/AstonJiuJitsu/".to_string(),
+					weekday: Weekday::Mon,
+					start: NaiveTime::from_hms(20, 30, 00),
+				},
+				Session {
+					name: "Brighton Jiu Jitsu Club".to_string(),
+					link: "https://www.facebook.com/groups/UniversitiesBrightonJitsu/".to_string(),
+					weekday: Weekday::Wed,
+					start: NaiveTime::from_hms(19, 00, 00),
+				}
+			]
+		})
+	}
 }
